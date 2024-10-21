e4ee4c510f91d923124968545de86d1082c087c5
Karthikeyan <165583967+Karthikeyan8296@users.noreply.github.com>
2024-10-21 14:21:47 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 className="text-blue-400">hello world</h1>
+    </div>
+  );
+}
+
+export default App;
